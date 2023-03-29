2da2e08e2718632261640b85997aea9487ea009f
diazdani <daniel.diaz@institutvidreres.cat>
2023-03-29 09:59:01 +0200

    commit examen

--- src/app/activitats/activitats.component.ts
+++ src/app/activitats/activitats.component.ts
@@ -8,6 +8,6 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ActivitatsComponent {
   constructor(private http: HttpClient) {
-
+    this.http.post("http://localhost:3080/api/ex1",{}).subscribe();
   }
 }
